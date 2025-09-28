66a88557f9086205a203ba5116f859448424e289
JyotiJain2002 <jyotijj2002@gmail.com>
2025-09-28 14:00:17 +0530

    variables commited

---
+++ python basics/escape.py
@@ -0,0 +1,17 @@
+#escape characater: some character are illegal to use in strings then you use those illegal characters in 
+#string you can you escape character   \ 
+# like in string double quotes in double quotes
+#you can use  backslash \ followed by the character you want to insert (illegal character).
+
+txt= " you know what it is a \"text\" "
+print(txt)
+
+
+#some other escape character
+# \\ -> will insert only one \ in text
+# \n -> new line
+# \r -> return only words after \r
+txt = "Hello\rWorld my lady!"
+print(txt) 
+
+#\t -> tab
---
+++ python basics/string_methods.py
@@ -0,0 +1,9 @@
+text="hello my world , its so good to see you here on such a auspicious occasion"
+print(text.capitalize()) #capitalize the first letter of the sentence
+print(text.casefold()) #Converts string into lower case
+print(text.endswith(" occasion"))
+print(text.index("occasion"))
+print(text.isalpha()) # false due to , and spaces
+print(text.isalnum()) # false due to , and spaces
+string = "heeltheworld"
+print(string.isalpha()) #true
