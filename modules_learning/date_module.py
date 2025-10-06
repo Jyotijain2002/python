14ac0dfbf0ea68380db0c7c49e8f54f0855aa453
JyotiJain2002 <jyotijj2002@gmail.com>
2025-10-06 09:28:40 +0530

    module commited

---
+++ modules_learning/date_module.py
@@ -0,0 +1,14 @@
+'''
+Python Dates
+A date in Python is not a data type of its own, but we can import a module named datetime to work with dates as date objects.
+'''
+
+import datetime 
+
+x=datetime.datetime.now().date().day
+print(x)
+x=datetime.datetime.now()
+print(x.year)
+print(x.ctime())
+print(x.hour)
+print(x.second)
